b2e5264a8f161fb68342f403f95851f1ebb03ac9
LSH005 <isihyeon005shingu@gmail.com>
2025-10-11 22:31:10 +0900

    [feat] 카메라 움직임 구현

---
+++ Assets/Script/Entity/Player/ThirdPersonCamera.cs
@@ -0,0 +1,68 @@
+﻿using Unity.PlasticSCM.Editor.WebApi;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    [Header("타겟 설정")]
+    public Transform target;
+    public Vector3 offset = new Vector3(0f, 0.5f, -5f);
+
+    [Header("제한 설정")]
+    public float minYAngle = -60f;
+    public float maxYAngle = 60f;
+
+    [Header("줌 설정")]
+    public float zoomSpeed = 40f;
+    public float maxZoomDistance = -2f;
+    public float minZoomDistance = -5f;
+
+    private float rotationX;
+    private float rotationY;
+    private float currentDistance;
+    private float rotationSpeed = 5f;
+
+    private void Start()
+    {
+        currentDistance = offset.z;
+    }
+
+    void Update()
+    {
+        if (target == null) return;
+
+        CameraPositionHandler();
+        CameraDistanceHandler();
+    }
+
+    void CameraPositionHandler()
+    {
+        rotationX += Input.GetAxis("Mouse X") * rotationSpeed;
+        rotationY -= Input.GetAxis("Mouse Y") * rotationSpeed;
+
+        rotationY = Mathf.Clamp(rotationY, minYAngle, maxYAngle);
+
+        Quaternion targetRotation = Quaternion.Euler(rotationY, rotationX, 0);
+
+        Vector3 finalPosition = target.position + targetRotation * offset;
+
+        transform.position = finalPosition;
+        transform.rotation = targetRotation;
+    }
+
+    void CameraDistanceHandler()
+    {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        if (scroll > 0)
+        {
+            currentDistance += zoomSpeed * Time.deltaTime;
+        }
+        else if (scroll < 0)
+        {
+            currentDistance -= zoomSpeed * Time.deltaTime;
+        }
+
+        currentDistance = Mathf.Clamp(currentDistance, minZoomDistance, maxZoomDistance);
+        offset.z = currentDistance;
+    }
+}
